ee853c88240f94aa11ba437df257378acbd43297
Sami <51018077+khalosamilo@users.noreply.github.com>
2023-01-23 15:13:48 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.post('/', async (req, res) => {
       model: "text-davinci-003",
       prompt: `Just follow the instructions. ${prompt}`, // ich muss hier noch beispiele geben oder so ich muss noch nachdenken
       temperature: 0.7, // Higher values means the model will take more risks.
-      max_tokens: max_tokens, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 1.2, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
