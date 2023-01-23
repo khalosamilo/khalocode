32f85877f11fd17edbadcb81a95fde21c2b90de5
Sami <51018077+khalosamilo@users.noreply.github.com>
2023-01-23 18:37:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,18 +25,12 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const longTextButton = document.getElementById("longTextButton");
-    longTextButton.addEventListener("click", increaseTokens);
-    let max_tokens = 100;
-    function increaseTokens() {
-      max_tokens = 100;
-  }
-
+    let max_tokens = 1000;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`, // ich muss hier noch beispiele geben oder so ich muss noch nachdenken
+      model: "text-davinci-003", 
+      prompt: "Summarize this for a second-grade student:\n\n"`${prompt}`, // ich muss hier noch beispiele geben oder so ich muss noch nachdenken
       temperature: 0.7, // Higher values means the model will take more risks.
-      max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: max_tokens, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 1.2, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
